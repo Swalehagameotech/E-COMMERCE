cf421be15e59959d58695c2f588ef4b88a256bbe
Swalehagameotech <zarekariswaleha@gmail.com>
2026-01-19 12:16:19 +0530

    chsnged some files

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://e-commerce-frontend-tzpq.onrender.com'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

0ffdf8793e4088ff36d5c1428b1d1a1852349fa4
Swalehagameotech <zarekariswaleha@gmail.com>
2026-02-21 15:50:20 +0530

    done-with chnage

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist'
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    build: {
+      outDir: 'dist'
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        }
       }
     }
   }
